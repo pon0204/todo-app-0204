35f43031062a42814c426466f62e55be848f6af9
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2020-10-18 19:59:30 +0900

    taskのedit destroyアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,8 +33,29 @@ def create
   end
 end
 
+def edit 
+  @board = Board.find(params[:board_id])
+  @task = @board.tasks.find(params[:id])
+end
 
-# /boards/:board_id/tasks(.:format)
+def update
+  @board = Board.find(params[:board_id])
+  @task = @board.tasks.find(params[:id])
+  if @task.update!(task_params)
+    redirect_to board_task_path(board_id: @board.id, id: @task.id), notice: 'タスクを追加'
+  else
+    flash.now[:error] = '更新できませんでした'
+    render :new
+  end
+end
+
+def destroy
+  @board = Board.find(params[:board_id])
+  @task = @board.tasks.find(params[:id])
+  @task.destroy! #記事の削除 !はデストロイ失敗した時にエラーがでて処理が止まる。
+  redirect_to board_path(@board), notice: '削除に成功しました' #削除後、記事一覧に飛ぶ
+end
+
 
 private
 def task_params
